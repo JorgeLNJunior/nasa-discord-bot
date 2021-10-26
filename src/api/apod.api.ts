163a837c80ec451e6e6fb022cce8a5f18daa4d42
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-10-26 16:26:10 -0300

    feat: criar o ApodApi

---
+++ src/api/apod.api.ts
@@ -0,0 +1,17 @@
+import { AxiosInstance } from 'axios';
+
+import { EnvService } from '../config/EnvService';
+import { ApodApiResponse } from './types/apod.types';
+
+export class ApodApi {
+  constructor(private envService: EnvService, private axios: AxiosInstance) {}
+
+  async find() {
+    const NASA_KEY = this.envService.get<string>('NASA_KEY');
+    const API_URL = 'https://api.nasa.gov/planetary/apod';
+
+    const response = await this.axios.get(`${API_URL}?api_key=${NASA_KEY}`);
+
+    return response.data as ApodApiResponse;
+  }
+}
