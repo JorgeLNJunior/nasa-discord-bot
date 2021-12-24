d438d28d44613ebdcbfd12b5af9a9933db3fc51f
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-12-24 15:00:13 -0300

    fix: corrigir as chamadas de api

--- src/api/apod.api.ts
+++ src/api/apod.api.ts
@@ -4,10 +4,10 @@ import { EnvService } from '../config/EnvService';
 import { ApodApiResponse } from './types/apod.types';
 
 export class ApodApi {
-  constructor(private envService: EnvService, private axios: AxiosInstance) {}
+  constructor(private envService: EnvService, private axios: AxiosInstance) { }
 
   async find() {
-    const NASA_KEY = this.envService.get<string>('NASA_KEY');
+    const NASA_KEY = this.envService.get<string>('NASA_API_KEY');
     const API_URL = 'https://api.nasa.gov/planetary/apod';
 
     const response = await this.axios.get(`${API_URL}?api_key=${NASA_KEY}`);
